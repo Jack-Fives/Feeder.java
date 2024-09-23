2b88da97f735bc70e53cbfb005cd23127e76f8ad
Jack-Fives <jackf58@nycstudents.net>
2024-09-23 12:14:57 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,6 +21,11 @@ public class Main
         Feeder i = new Feeder(100);
         i.simulateOneDay(5);
         System.out.println(i);
+        Feeder j = new Feeder(2400);
+        System.out.println(j.simulateManyDays(10, 4));
+        Feeder k = new Feeder(0);
+        System.out.println(k.simulateManyDays(5, 10));
+
 
 
     }
